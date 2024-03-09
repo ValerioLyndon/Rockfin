7bc9c3103fbbdcdbfd3a181f0547b04654bfb5ee
Valerio Lyndon <valeriolyndon@gmail.com>
2024-03-09 15:03:39 -0800

    script to combine separate files

--- readme.md
+++ readme.md
@@ -2,6 +2,10 @@
 
 This project is in an **unreleased** state. Expect bugs and bare minimum QC for commits until it's more feature complete.
 
+## Installation
+
+Currently, all the recommended or "core" CSS is combined into the ["recommended.css"](recommended.css) file in this directory. You can copy-paste that entire file into your Jellyfin CSS box. See how to do that [here](https://jellyfin.org/docs/general/clients/css-customization/).
+
 ## Gallery
 
 These screenshots display designs that may have already changed or be subject to change later.
---
+++ scripts/combine.py
@@ -0,0 +1,19 @@
+import os
+
+def concatenate_css_files(directory, output_file, custom_order=None):
+    if not os.path.exists(directory):
+        print(f"Error: Directory '{directory}' does not exist.")
+        return
+    
+    css_files = os.listdir(directory)
+    
+    # Sort the files based on the custom order
+    if custom_order:
+        css_files.sort(key=lambda x: custom_order.index(x) if x in custom_order else float('inf'))
+    
+    with open(output_file, 'w') as outfile:
+        for css_file in css_files:
+            with open(os.path.join(directory, css_file), 'r') as infile:
+                outfile.write(infile.read())
+
+concatenate_css_files("../core", "../recommended.css", custom_order=["base.css"])
